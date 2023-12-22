921e3c2d68cb98fc32817fe9cbe4cbec5ac53ba2
Mike van den Hoek <mike@yard.nl>
2023-12-22 12:05:01 +0100

    (feat): use decrypted value of prepopulated fields containing a BSN number when hook 'owc_prefill_gravityforms_use_value_bsn_decrypted' is used and set to true.

--- src/PrefillGravityForms/GravityForms/GravityForms.php
+++ src/PrefillGravityForms/GravityForms/GravityForms.php
@@ -2,11 +2,16 @@
 
 namespace OWC\PrefillGravityForms\GravityForms;
 
+use GF_Field;
+use GFAPI;
 use function OWC\PrefillGravityForms\Foundation\Helpers\get_supplier;
+use function Yard\DigiD\Foundation\Helpers\decrypt;
+use function Yard\DigiD\Foundation\Helpers\encrypt;
 
 class GravityForms
 {
     protected string $supplier;
+    protected bool $shouldDecrypt;
 
     public function preRender(array $form): array
     {
@@ -54,5 +59,66 @@ class GravityForms
         }
 
         return new $controller();
+    }
+
+    /**
+     * For security reasons, when populating/prefilling a field with a BSN number, the value is encrypted and securely stored.
+     */
+    public function saveFieldValue(string $value, $lead, GF_Field $field, array $form): string
+    {
+        if ('burgerservicenummer' !== ($field->linkedFieldValue ?? '')) {
+            return $value;
+        }
+
+        if (empty($value) || ! is_string($value)) {
+            return $value;
+        }
+
+        return encrypt($value);
+    }
+
+    /**
+     * Decrypts the value for display on the Entry list page, only for prepopulated fields containing a BSN number.
+     */
+    public function modifyEntryValue(string $value, int $formID, int $fieldID): string
+    {
+        $field = GFAPI::get_field($formID, $fieldID);
+
+        if (empty($field->linkedFieldValue) || 'burgerservicenummer' !== ($field->linkedFieldValue ?? '')) {
+            return $value;
+        }
+
+        $shouldDecrypt = apply_filters('owc_prefill_gravityforms_use_value_bsn_decrypted', false);
+
+        if ($shouldDecrypt) {
+            $value = $this->decryptEncryptedBSN($value);
+        }
+
+        return esc_html($value);
+    }
+
+    /**
+     * Decrypts the value for display on the Entry detail page, only for prepopulated fields containing a BSN number.
+     */
+    public function modifyEntryValueDetail($value, $field, $lead, $form): string
+    {
+        if (empty($field->linkedFieldValue) || 'burgerservicenummer' !== ($field->linkedFieldValue ?? '')) {
+            return $value;
+        }
+
+        $shouldDecrypt = apply_filters('owc_prefill_gravityforms_use_value_bsn_decrypted', false);
+
+        if ($shouldDecrypt) {
+            $value = $this->decryptEncryptedBSN($value);
+        }
+
+        return esc_html($value);
+    }
+
+    private function decryptEncryptedBSN(string $value): string
+    {
+        $decrypted = decrypt($value);
+
+        return $decrypted && is_string($decrypted) ? $decrypted : $value;
     }
 }
--- src/PrefillGravityForms/GravityForms/GravityFormsServiceProvider.php
+++ src/PrefillGravityForms/GravityForms/GravityFormsServiceProvider.php
@@ -20,11 +20,15 @@ class GravityFormsServiceProvider extends ServiceProvider
     {
         $gravityFormsFieldSettings = new GravityFormsFieldSettings();
         $gravityFormsFormSettings = new GravityFormsFormSettings();
+        $gravityForms = new GravityForms();
 
-        $this->plugin->loader->addFilter('gform_pre_render', new GravityForms(), 'preRender');
+        $this->plugin->loader->addFilter('gform_pre_render', $gravityForms, 'preRender');
         $this->plugin->loader->addAction('gform_field_standard_settings', $gravityFormsFieldSettings, 'addSelect', 10, 2);
         $this->plugin->loader->addAction('gform_editor_js', $gravityFormsFieldSettings, 'addSelectScript', 10, 0);
         $this->plugin->loader->addFilter('gform_form_settings_fields', $gravityFormsFormSettings, 'addFormSettings', 9999, 2);
+        $this->plugin->loader->addAction('gform_save_field_value', $gravityForms, 'saveFieldValue', 10, 4);
+        $this->plugin->loader->addFilter('gform_entries_field_value', $gravityForms, 'modifyEntryValue', 10, 3);
+        $this->plugin->loader->addFilter('gform_entry_field_value', $gravityForms, 'modifyEntryValueDetail', 10, 4);
     }
 
     private function registerSettingsAddon(): void
