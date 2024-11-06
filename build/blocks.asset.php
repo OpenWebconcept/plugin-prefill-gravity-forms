17baba92a211bc328b220f91a28ae1906dece6f1
Mike van den Hoek <mike@yard.nl>
2024-11-06 14:53:20 +0100

    (chore): run asset build

--- build/blocks.asset.php
+++ build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cdfaeebe4b64af07339c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '33b24b2f8a8b7ff4f915');
