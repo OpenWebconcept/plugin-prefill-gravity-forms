2a4cfd1072994a8e657a5962984293272b7cef4c
Mike van den Hoek <mike@yard.nl>
2026-01-08 16:20:26 +0100

    refactor: update block registration + prefix to make blocks visible in editor

--- build/personal-data-table/personal-data-row/index.asset.php
+++ build/personal-data-table/personal-data-row/index.asset.php
@@ -1,1 +1,1 @@
-<?php return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'], 'version' => 'be42ab64f714d867fcc6'];
+<?php return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'], 'version' => 'b973b3662f9ee0c4c3e3'];
