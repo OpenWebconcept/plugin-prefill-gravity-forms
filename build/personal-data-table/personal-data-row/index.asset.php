417fe16e3557756e5f0189d33d5a4ee4ccc7c2a3
Mike van den Hoek <mike.vandenhoek@yard.nl>
2026-02-05 08:17:19 +0100

    fix: personal data row inner block

--- build/personal-data-table/personal-data-row/index.asset.php
+++ build/personal-data-table/personal-data-row/index.asset.php
@@ -1,1 +1,1 @@
-<?php return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'], 'version' => 'b973b3662f9ee0c4c3e3'];
+<?php return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'], 'version' => 'f2161e666b36d62f2bfb'];
