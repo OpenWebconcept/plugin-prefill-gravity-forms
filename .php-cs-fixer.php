fa198de02c0e82a559461cc13ba5b14932a87618
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2026-02-27 12:59:24 +0100

    refactor: pr comments

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,39 +3,40 @@
 declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
-	->notPath('vendor')
-	->notPath('node_modules')
-	->in(__DIR__)
-	->in('./resources/views')
-	->in('./config')
-	->name('*.php')
-	->notName('*.blade.php')
-	->ignoreDotFiles(true)
-	->ignoreVCS(true);
+    ->notPath('vendor')
+    ->notPath('node_modules')
+    ->in(__DIR__)
+    ->in('./resources/views')
+    ->in('./config')
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
 
 return (new PhpCsFixer\Config)
-	->setRules([
-		'@PSR2' => true,
-		'array_syntax' => [
-			'syntax' => 'short',
-		],
-		'ordered_imports' => [
-			'sort_algorithm' => 'alpha',
-		],
-		'no_unused_imports' => true,
-		'binary_operator_spaces' => [
-			'default' => 'single_space',
-			'operators' => [
-				'=>' => 'single_space',
-				'|' => 'no_space',
-			],
-		],
-		'full_opening_tag' => true,
-		'yoda_style' => [
-			'always_move_variable' => true,
-			'equal' => true,
-			'identical' => true,
-			'less_and_greater' => true,
-		],
-	])
-	->setFinder($finder);
+    ->setRules([
+        '@PSR2' => true,
+        'array_syntax' => [
+            'syntax' => 'short',
+        ],
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+        ],
+        'no_unused_imports' => true,
+        'array_indentation' => true,
+        'binary_operator_spaces' => [
+            'default' => 'single_space',
+            'operators' => [
+                '=>' => 'single_space',
+                '|' => 'no_space',
+            ],
+        ],
+        'full_opening_tag' => true,
+        'yoda_style' => [
+            'always_move_variable' => true,
+            'equal' => true,
+            'identical' => true,
+            'less_and_greater' => true,
+        ],
+    ])
+    ->setFinder($finder);
